c1f4474c94446888243eacfb87506fc3e60af3ca
Erioluwa Adeola <erioluwa97@gmail.com>
2019-06-13 12:00:39 +0100

    Ricky_B

---
+++ Online Point Of Sale System/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Online_Point_Of_Sale_System.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
